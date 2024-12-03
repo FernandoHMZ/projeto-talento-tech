aa6a74725f6835542ae6b14c16d26b308fa4e257
FernandoHMZ <fernando.henriquemunhozda.rocha@gmail.com>
2024-12-03 15:21:54 -0300

    Salvando alterações

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let currentExpression = '';
+let isScientific = false;  // Variável para controlar o modo da calculadora
 
 function appendNumber(value) {
   currentExpression += value;
@@ -23,4 +24,18 @@ function calculateResult() {
 
 function setTheme(theme) {
     document.body.className = theme; // Define a classe do body como o tema selecionado
-  }  
+}
+
+function toggleCalculatorMode() {
+    // Alterna entre o modo simples e o científico
+    isScientific = !isScientific;
+
+    // Mostrar ou esconder os botões de acordo com o modo
+    if (isScientific) {
+        document.getElementById('simple-buttons').style.display = 'none';
+        document.getElementById('scientific-buttons').style.display = 'block';
+    } else {
+        document.getElementById('simple-buttons').style.display = 'block';
+        document.getElementById('scientific-buttons').style.display = 'none';
+    }
+}
